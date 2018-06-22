ee1255a759bd9b03dfde102082edf809ce49a8c2
Simo Antikainen <simo.antikainen@aalto.fi>
2018-06-22 18:56:20 +0300

    part 3.11

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
 
+app.use(express.static('build'))
 app.use(cors())
-
 app.use(bodyParser.json())
 
 morgan.token('type', function (req, res) { return JSON.stringify(req.body)})
